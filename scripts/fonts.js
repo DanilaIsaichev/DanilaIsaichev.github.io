1e8ec69fecf7ff42fff78ae6354a5b2a2dd1130e
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2021-02-09 22:58:25 +0300

    Update fonts.js

--- scripts/fonts.js
+++ scripts/fonts.js
@@ -1,2 +1,2 @@
 var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg'];
-document.body.style.background = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ') center / 100% no-repeat fixed';
+document.body.style.background = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ') center / cover no-repeat fixed';
