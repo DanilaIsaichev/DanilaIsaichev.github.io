1a46d5b963e761bfe0c843b3b97f23e80395c60f
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2021-02-10 22:46:04 +0300

    Update fonts.js

--- scripts/fonts.js
+++ scripts/fonts.js
@@ -1,3 +1,5 @@
+document.body.style.margin = '0';
+document.body.style.backgroundColor = 'rgb(253,245,220)';
 var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg'];
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     document.body.style.background = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ') center / cover no-repeat fixed';
