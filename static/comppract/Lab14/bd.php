0effcd824bf9f58d982a95b6afa7a6ef212677f1
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2023-05-29 00:45:39 +0300

    Delete static/comppract/Lab14 directory

--- static/comppract/Lab14/bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbloc = 'localhost';
-    $dbuser = $dbname = 'user1';
-    $dbpass = 'Qwerty123';
-    $dsn = $dsn = "mysql:host={$dbloc};dbname={$dbname}";
-    return new PDO($dsn, $dbuser, $dbpass);
-?>
