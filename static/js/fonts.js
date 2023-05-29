2fd7c4a80baf0b10f82e739da6f89d8bba6923fd
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2023-05-29 10:40:23 +0300

    Update fonts.js

--- static/js/fonts.js
+++ static/js/fonts.js
@@ -1,6 +1,6 @@
 document.body.style.margin = '0';
 document.body.style.backgroundColor = 'rgb(253,245,220)';
-var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg'];
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg'];
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     document.body.style.background = 'url(../images/' + images[Math.floor(Math.random() * images.length)] + ') center / cover no-repeat fixed';
 } else document.body.style.background = 'url(../images/' + images[Math.floor(Math.random() * images.length)] + ') center center / auto 100% no-repeat fixed';
