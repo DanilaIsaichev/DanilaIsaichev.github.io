3028f783a1b344edc2bac51d643eb8a3cf2f9334
DanilaIsaichev <DanilaIsaichev@users.noreply.github.com>
2023-05-29 23:37:35 +0000

    deploy: a9e29233eeb8d375a20e91667c0ac5f90d1277a4

--- comppract/cp/vsr/t1/index.js
+++
@@ -1,4 +0,0 @@
-import moment from 'moment';
-const t = moment().format('DD/MM/YYYY HH:mm');
-document.body.innerHTML = `<h1>Сейчас ${t}</h1>`;
-document.body.innerHTML += `<h2>${top.location.href}</h2>`;
